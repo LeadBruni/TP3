d4745d726bfcdb73212744edff02b34f08c89b60
SSmash <SSmash@MSI>
2016-09-28 19:15:45 -0300

    primera version

---
+++ TP3/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    class Animal
+    {
+        public void Correr() { Console.WriteLine("Corriendo"); }
+        public void Saltar() { Console.WriteLine("Saltando"); }
+        public virtual void HacerRuido() { }
+    }
+}
