2894edc96969e59e96fa15f6fe919b7546e66e49
LeadBruni <leadbruni@hotmail.com>
2016-09-30 19:45:24 -0300

    corregido lo de solicitud prestamo

--- Tp3_ej3/SolicitudPrestamo.cs
+++ Tp3_ej3/SolicitudPrestamo.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace TP3
 {
-    class SolicitudPrestamos
+    class SolicitudPrestamo
     {
+        //Constructores
         private double iMonto;
         private int iCantidadCuotas;
         private Cliente iCliente;
 
+        //Properties
         public double Monto { get { return this.iMonto; } }
         public int CantidadCuotas { get { return this.iCantidadCuotas; } }
 
+
         public SolicitudPrestamos(Cliente pCliente, double pMonto,int pCantidadCuotas)
+            {
+            this.iCliente = pCliente;
+            this.iMonto = pMonto;
+            this.iCantidadCuotas = pCantidadCuotas;
+            }
+
 
     }
 }
