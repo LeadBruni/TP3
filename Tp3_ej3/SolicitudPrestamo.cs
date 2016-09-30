bf75763079deb3548404a64656e22a8177c21936
LeadBruni <leadbruni@hotmail.com>
2016-09-30 19:30:32 -0300

    Solicitud Cliente constructores y algunas cosas mas medio rotas y arreglo en lo del ejercicio 1

---
+++ Tp3_ej3/SolicitudPrestamo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    class SolicitudPrestamos
+    {
+        private double iMonto;
+        private int iCantidadCuotas;
+        private Cliente iCliente;
+
+        public double Monto { get { return this.iMonto; } }
+        public int CantidadCuotas { get { return this.iCantidadCuotas; } }
+
+        public SolicitudPrestamos(Cliente pCliente, double pMonto,int pCantidadCuotas)
+
+    }
+}
+
