e0242c35c4ff4f6461aee0b21e421b46cf4a0311
SSmash <SSmash@MSI>
2016-09-30 18:21:25 -0300

    prueba 2

---
+++ Tp3_ej3/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3_ej3
+{
+    class Cliente
+    {
+        private string iNombre;
+        private string iApellido;
+        private DateTime iFechaNacimiento;
+
+
+    }
+}
