80126aefccb1d3cc63d0a0467bff872f7bb0a5bd
emanuel bidal <bidalemanuel@gmail.com>
2016-09-30 19:42:45 -0300

    Agregue IEvaluador

---
+++ Tp3_ej3/IEvaluador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3_ej3
+{
+    public interface IEvaluador
+    {
+        public bool EsValida(SolicitudEmpleado pSolicitud)
+        {
+
+        }
+    }
+}
