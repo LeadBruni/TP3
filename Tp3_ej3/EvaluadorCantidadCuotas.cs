b14daafe63daccf17bbfacdbeffa821dbe3f6b07
LeadBruni <leadbruni@hotmail.com>
2016-10-05 18:46:02 -0300

    Agregado EvaluadorSueldo y EvaluadorCantidadCuotas by Poyo

---
+++ Tp3_ej3/EvaluadorCantidadCuotas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3_ej3
+{
+    public class EvaluadorCantidadCuotas:IEvaluador
+    {
+        private int iCantidadMaximaCuotas;
+        public EvaluadorCantidadCuotas(int pCantidadMaxima)
+        {
+            this.iCantidadMaximaCuotas = pCantidadMaxima;
+        }
+
+        public bool EsValida(SolicitudPrestamo pSolicitud)
+        {
+
+            bool pResultado = false;
+            if (iCantidadMaximaCuotas >= pSolicitud.CantidadCuotas) { pResultado = true; }
+            return pResultado;
+
+        }
+    }
+}
