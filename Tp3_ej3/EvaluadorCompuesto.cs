dda1034eacad2783938075115bec8248f1b400f6
MAURICIO <Acer@ACER-PC>
2016-10-06 09:29:56 -0300

    cambios clase de miercoles

--- Tp3_ej3/EvaluadorCompuesto.cs
+++ Tp3_ej3/EvaluadorCompuesto.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace Tp3_ej3
 {
-    class EvaluadorCompuesto
+    public class EvaluadorCompuesto : IEvaluador
     {
 
-        private List<IEvaluador> iEvaluadores  ;
+        private readonly List<IEvaluador> iEvaluadores;
 
+        public EvaluadorCompuesto()
+        { this.iEvaluadores = new List<IEvaluador>(); }
 
-        public EvaluadorCompuesto() { new List<IEvaluador>(iEvaluadores); }
+        public void AgregarEvaluador(IEvaluador pEvaluador)
+        {
+            this.iEvaluadores.Add(pEvaluador);
+        }
+
+        public bool EsValida(SolicitudPrestamo pSolicitud)
+        {
+            bool esValida = true;
+
+            IEnumerator<IEvaluador> enumerador = this.iEvaluadores.GetEnumerator();
+
+            while (esValida && enumerador.MoveNext())
+            {
+                esValida = enumerador.Current.EsValida(pSolicitud);
+            }
+
+            return esValida;
+        }
 
 
 
