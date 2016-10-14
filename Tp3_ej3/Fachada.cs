9f072a3b38f3a7f45c8ee5c6182a859824faf1c5
LeadBruni <leadbruni@hotmail.com>
2016-10-14 15:25:00 -0300

    fachada retocado

--- Tp3_ej3/Fachada.cs
+++ Tp3_ej3/Fachada.cs
@@ -22,5 +22,25 @@ namespace Tp3_ej3
             return false;
         }
 
+        public Empleo IngresarEmpleo(double pSueldo, DateTime pFechaIngreso)
+        {
+            return new Empleo(pSueldo, pFechaIngreso);
+        }
+        //Por defecto el Cliente creado es del tipo NoCliente
+        public Cliente IngresarCliente(string pNombre, string pApellido, DateTime pFechaNac, Empleo pEmpleo)
+        {
+            return new Cliente(pNombre, pApellido, pFechaNac, pEmpleo);
+        }
+
+        public SolicitudPrestamo IngresarSolicidudPrestamo(Cliente pCliente, double pMonto, int pCantidadCuotas)
+        {
+            return new SolicitudPrestamo(pCliente, pMonto, pCantidadCuotas );
+        }
+
+        public bool ControlarSolicitud(SolicitudPrestamo pSolicitudPrestamo)
+        {
+            return new GestorPrestamo().EsValida(pSolicitudPrestamo);
+
+        }
     }
 }
