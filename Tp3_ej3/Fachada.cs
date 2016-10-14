b0f4efccd2fa1b53e5d77575d5d45efd121517b4
LeadBruni <leadbruni@hotmail.com>
2016-10-14 15:20:53 -0300

    fachada y cliente automaticamente a noCliente

---
+++ Tp3_ej3/Fachada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp3_ej3
+{
+    class Fachada
+    {
+        Cliente fCliente;
+        Empleo fEmpleo;
+        SolicitudPrestamo fSolicitud;
+        TipoCliente fTipoCliente;
+        IEvaluador fIEvaluador;
+
+        //Cliente Cliente = new Cliente;
+
+        public bool EsValida(SolicitudPrestamo fSolicitud)
+        {
+            fIEvaluador.EsValida(fSolicitud);
+            return false;
+        }
+
+    }
+}
