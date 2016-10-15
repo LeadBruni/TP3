dc7c52cd29a4bb12bce71b40a8894f3eb0d1e99b
emanuel bidal <bidalemanuel@gmail.com>
2016-10-14 21:35:34 -0300

    ultima version

--- Tp3_ej3/Fachada.cs
+++ Tp3_ej3/Fachada.cs
@@ -12,11 +12,11 @@ namespace Tp3_ej3
         Empleo fEmpleo;
         SolicitudPrestamo fSolicitud;
         TipoCliente fTipoCliente;
-       
+
 
         //Cliente Cliente = new Cliente;
 
-       
+
 
         public Empleo IngresarEmpleo(double pSueldo, DateTime pFechaIngreso)
         {
@@ -30,13 +30,55 @@ namespace Tp3_ej3
 
         public SolicitudPrestamo IngresarSolicitudPrestamo(Cliente pCliente, double pMonto, int pCantidadCuotas)
         {
-            return new SolicitudPrestamo(pCliente, pMonto, pCantidadCuotas );
+            return new SolicitudPrestamo(pCliente, pMonto, pCantidadCuotas);
         }
 
         public bool ControlarSolicitud(SolicitudPrestamo pSolicitudPrestamo)
         {
             return new GestorPrestamo().EsValida(pSolicitudPrestamo);
 
+        }
+        public bool ingresarDatosPorCliente(int pOpcionTipoCliente, string pNombre, string pApellido, DateTime pFechaNacimiento, double pSueldo, DateTime pFechaIngreso, double pMonto, int pCantidadCuotas)
+        {
+            Empleo pEmpleo = this.IngresarEmpleo(pSueldo, pFechaIngreso);
+            bool mResultado = false;
+            switch (pOpcionTipoCliente)
+            {
+                case 1:
+                    {
+                        Cliente pCliente = this.IngresarCliente(pNombre, pApellido, pFechaNacimiento, pEmpleo);
+                        pCliente.TipoCliente = TipoCliente.NoCliente;
+                        SolicitudPrestamo pSolicitud = this.IngresarSolicitudPrestamo(pCliente, pMonto, pCantidadCuotas);
+                         mResultado = this.ControlarSolicitud(pSolicitud);
+                        break;
+                    }
+                case 2:
+                    {
+                        Cliente pCliente = this.IngresarCliente(pNombre, pApellido, pFechaNacimiento, pEmpleo);
+                        pCliente.TipoCliente = TipoCliente.Cliente;
+                        SolicitudPrestamo pSolicitud = this.IngresarSolicitudPrestamo(pCliente, pMonto, pCantidadCuotas);
+                        mResultado = this.ControlarSolicitud(pSolicitud);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        Cliente pCliente = this.IngresarCliente(pNombre, pApellido, pFechaNacimiento, pEmpleo);
+                        pCliente.TipoCliente = TipoCliente.ClienteGold;
+                        SolicitudPrestamo pSolicitud = this.IngresarSolicitudPrestamo(pCliente, pMonto, pCantidadCuotas);
+                        mResultado = this.ControlarSolicitud(pSolicitud);
+                        break;
+                    }
+                case 4:
+                    {
+                        Cliente pCliente = this.IngresarCliente(pNombre, pApellido, pFechaNacimiento, pEmpleo);
+                        pCliente.TipoCliente = TipoCliente.ClientePlatinum;
+                        SolicitudPrestamo pSolicitud = this.IngresarSolicitudPrestamo(pCliente, pMonto, pCantidadCuotas);
+                        mResultado = this.ControlarSolicitud(pSolicitud);
+                        break;
+                    }
+            }
+            return mResultado;
         }
     }
 }
--- Tp3_ej3/Program.cs
+++ Tp3_ej3/Program.cs
@@ -13,78 +13,34 @@ namespace Tp3_ej3
             Fachada fachadaSolicitud = new Fachada();
             Console.WriteLine("Ingrese su nombre");
             string nombre = Console.ReadLine();
+
             Console.WriteLine("Ingrese su apellido");
             string apellido = Console.ReadLine();
+
             Console.WriteLine("Elija el tipo de cliente que es: ");
             Console.WriteLine("1-No Cliente  2-Cliente 3-Cliente Gold 4-Cliente Platinum");
             int opcion = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Ingrese fecha de nacimiento");
             DateTime fNacimiento = Convert.ToDateTime(Console.ReadLine());
+
             Console.WriteLine("Ingrese fecha ingreso a empleo");
             DateTime fIngreso = Convert.ToDateTime(Console.ReadLine());
+
             Console.WriteLine("Ingrese el sueldo mensual de su empleo");
             double sueldo = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("ingrese monto a solicitar");
             double monto = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("ingrese cantidad de cuotas");
             int cuotas = Convert.ToInt32(Console.ReadLine());
-            Empleo empleo = fachadaSolicitud.IngresarEmpleo(sueldo, fIngreso);
 
-            switch (opcion)
-            {
-                case 1:
-                    {
-                        Cliente cliente = fachadaSolicitud.IngresarCliente(nombre, apellido, fNacimiento, empleo);
-                        cliente.TipoCliente = TipoCliente.NoCliente;
-                        SolicitudPrestamo solicitud = fachadaSolicitud.IngresarSolicitudPrestamo(cliente, monto, cuotas);
-                        bool mResultado = fachadaSolicitud.ControlarSolicitud(solicitud);
-                        if (mResultado)
-                        { Console.WriteLine("Solicitud Valida"); }
-                        else { Console.WriteLine("Solicitud Invalida"); }
-
-                        Console.ReadKey();
-                        break;
-                    }
-                case 2:
-                    {
-                        Cliente cliente = fachadaSolicitud.IngresarCliente(nombre, apellido, fNacimiento, empleo);
-                        cliente.TipoCliente = TipoCliente.Cliente;
-                        SolicitudPrestamo solicitud = fachadaSolicitud.IngresarSolicitudPrestamo(cliente, monto, cuotas);
-                        bool mResultado = fachadaSolicitud.ControlarSolicitud(solicitud);
-                        if (mResultado)
-                        { Console.WriteLine("Solicitud Valida"); }
-                        else { Console.WriteLine("Solicitud Invalida"); }
-
-                        Console.ReadKey();
-                        break;
-                    }
-                case 3:
-                    {
-                        Cliente cliente = fachadaSolicitud.IngresarCliente(nombre, apellido, fNacimiento, empleo);
-                        cliente.TipoCliente = TipoCliente.ClienteGold;
-                        SolicitudPrestamo solicitud = fachadaSolicitud.IngresarSolicitudPrestamo(cliente, monto, cuotas);
-                        bool mResultado = fachadaSolicitud.ControlarSolicitud(solicitud);
-                        if (mResultado)
-                        { Console.WriteLine("Solicitud Valida"); }
-                        else { Console.WriteLine("Solicitud Invalida"); }
-
-                        Console.ReadKey();
-                        break;
-                    }
-                case 4:
-                    {
-                        Cliente cliente = fachadaSolicitud.IngresarCliente(nombre, apellido, fNacimiento, empleo);
-                        cliente.TipoCliente = TipoCliente.ClientePlatinum;
-                        SolicitudPrestamo solicitud = fachadaSolicitud.IngresarSolicitudPrestamo(cliente, monto, cuotas);
-                        bool mResultado = fachadaSolicitud.ControlarSolicitud(solicitud);
-                        if (mResultado)
-                        { Console.WriteLine("Solicitud Valida"); }
-                        else { Console.WriteLine("Solicitud Invalida"); }
-
-                        Console.ReadKey();
-                        break;
-                    }
-            }
+            bool resultado = false;
+            resultado = fachadaSolicitud.ingresarDatosPorCliente(opcion,nombre, apellido, fNacimiento, sueldo, fIngreso, monto, cuotas);
+            if (resultado) { Console.WriteLine("Solicitud Valida"); }
+            else { Console.WriteLine("Solicitud Invalida "); }
+            Console.ReadKey();
         }
     }
 }
